3ff77b54f2c52c1eed8504961fdc4ef17e5c51cf
dilaaslan3 <dilaaslan0144@gmail.com>
2020-11-06 15:58:52 +0300

    added all score metrics

--- linear_reg.py
+++ linear_reg.py
@@ -1,23 +1,23 @@
 import numpy as np
 
+
 class LinearRegression:
     def __init__(self, lr=0.01, n_iters=1000):
         self.lr = lr
         self.n_iters = n_iters
         self.weigths = None
         self.bias = None,
-    
+
     def fit(self, X, y):
-        #we need to initiliaze the bias and weigths parameters
+        # we need to initiliaze the bias and weigths parameters
         n_samples, n_features = X.shape
-        print(n_features)
-        self.weigths = np.zeros(n_features) #set the zeros for each coefficient of x
-        self.bias = 0 
-        
-        #gradient descent
+        # set the zeros for each coefficient of x
+        self.weigths = np.zeros(n_features)
+        self.bias = 0
+
+        # gradient descent
         for _ in range(self.n_iters):
-            y_pred = np.dot(X, self.weigths) + self.bias
-            
+            y_pred = self.predict(X)
             dw = (1/n_samples) * np.dot(X.T, (y_pred - y))
             db = (1/n_samples) * np.sum(y_pred-y)
 
@@ -25,8 +25,12 @@ class LinearRegression:
             self.bias -= self.lr*db
 
     def predict(self, X):
-        y_pred = np.dot(X, self.weigths) + self.bias
-        return y_pred
+        return np.dot(X, self.weigths) + self.bias
 
-    def score(self, y, y_pred): #mean squared error
-        return np.mean((y-y_pred)**2)
+    def score(self, y, y_pred):
+        mse = np.mean((y-y_pred)**2)  # mean squared error
+        mae = np.mean(abs(y-y_pred))  # mean absolute error
+        sqrte = np.sqrt(mse)  # root mean squared error
+        r2 = 1-(sum((y-y_pred)**2)/sum((y-np.mean(y))**2))  # r-squared error
+
+        return mse, mae, sqrte, r2
--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn import datasets
 import argparse
 from linear_reg import LinearRegression
+
 
 def main():
 
@@ -12,16 +11,21 @@ def main():
     X = dataset.iloc[:, :-1].values
     y = dataset.iloc[:, 1].values
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
 
-    regressor = LinearRegression()
+    regressor = LinearRegression(float(args.lr), int(args.n_iter))
     regressor.fit(X_train, y_train)
     predicted = regressor.predict(X_test)
-    mse_value = regressor.score(y_test, predicted)
-    print("score is: ", mse_value)
+    mse_value, mae_value, msqrte_value, rsqrte_value = regressor.score(
+        y_test, predicted)
+    print("Mean Squared Error score is: ", mse_value)
+    print("Mean Absolute Error score is: ", mae_value)
+    print("Root Mean Absolute Error score is: ", msqrte_value)
+    print("R-Squared Error score is: ", rsqrte_value)
 
     y_pred_line = regressor.predict(X)
-    plt.figure(figsize=(8,6))
+    plt.figure(figsize=(8, 6))
     plt.scatter(X_train, y_train, color='b', s=10)
     plt.scatter(X_test, y_test, color='r', s=10)
     plt.plot(X, y_pred_line, color='black', linewidth=2, label="Prediction")
@@ -31,10 +35,7 @@ def main():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-file_name", help="file_name")
+    parser.add_argument('-lr', help="learning rate, float")
+    parser.add_argument("-n_iter", help="number of iterations, int")
     args = parser.parse_args()
     main()
-
-
-
-
-
