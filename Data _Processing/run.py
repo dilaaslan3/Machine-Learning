6ee8553e2260bcaecfb3306e9135a6dbe91137bd
Dila Aslan <dila.aslan@sabancidx.com>
2020-11-12 23:40:34 +0300

    completed DataProvider class

--- Data _Processing/run.py
+++ Data _Processing/run.py
@@ -1,47 +1,20 @@
-import pandas as pd
 import argparse
-from sklearn.model_selection import train_test_split
-
-
-def read(path):
-
-    df = pd.read_csv(path)
-    print(df.head())
-    print("Shape of the dataset: \n", df.shape)
-    print("Information of the dataset: \n", df.info())
-
-    # null checkpoint
-    print(df.isnull().sum())
-    return df
-
-
-def one_hot_encoding(df):
-    categorical_features = list((df.select_dtypes(include=['object'])).columns)
-    X = pd.get_dummies(df, columns=categorical_features, drop_first=True)
-    return X
-
-
-def split(df):
-    X = df.drop(["price"], axis=1)
-    y = df["price"]
-
-    # TO DO: split data from scratch
-    return train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-def normalize():
-    pass
+from Data_Provider import DataProvider
 
 
 def main():
-    df = read(f'./{args.dataset}')
-    X = one_hot_encoding(df)
-    X_train, X_test, y_train, y_test = split(X)
-    normalize()
+    provider = DataProvider()
+    df = provider.read(f'./{args.dataset}')
+    X = provider.one_hot_encoding(df)
+    X_train, X_validation, X_test, y_train, y_validation, y_test = provider.split(X, args.train_ratio)
+    X_train_df_mean, X_train_df_std, X_train_norm, X_validation_norm, X_test_norm = provider.normalize(X_train, X_validation, X_test)
+    provider.save(X_train_df_mean, X_train_df_std)
+    y_train_df_mean, y_train_df_std, y_train_norm, y_validation_norm, y_test_norm = provider.normalize(y_train, y_validation, y_test)
 
 
 if __name__ == "__main__":
     data_parser = argparse.ArgumentParser()
     data_parser.add_argument("-dataset", type=str, required=True, help="Enter the dataset file name.")
+    data_parser.add_argument("-train_ratio", type=float, required=False, default=0.6, help="Train set splitting")
     args = data_parser.parse_args()
     main()
